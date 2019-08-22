d284a2607be308d418c294bcc0fbcdd13de7608f
Felix <ShimizuShiori@outlook.com>
2019-08-22 10:01:01 +0800

    添加 demo 项目，用于演示

---
+++ src/DotNet/Reface.EventBus/IPrioritized.cs
@@ -0,0 +1,13 @@
+﻿namespace Reface.EventBus
+{
+    /// <summary>
+    /// 表示事件允许执行一个值来确定它的执行顺序
+    /// </summary>
+    public interface IPrioritized
+    {
+        /// <summary>
+        /// 优先级，执行时将会按照从小到大的顺序进行执行
+        /// </summary>
+        int Priority { get; }
+    }
+}
