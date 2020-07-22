34d2221d44996a7f77f2e2993edf7885cf5d407c
ShimizuShiori <116971263@qq.com>
2020-07-22 10:58:19 +0800

    编写了基于程序集的监听器收集器

---
+++ src/DotNet/Reface.EventBus/EventListenerFinders/AssembliesEventListenerTypeFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Reface.EventBus.EventListenerFinders
+{
+    public class AssembliesEventListenerTypeFinder : IEventListenerTypeFinder
+    {
+        private readonly IEventListenerTypeFinder defaultFinder;
+        private readonly static Type TYPE_LISTENER = typeof(IEventListener);
+
+
+        public AssembliesEventListenerTypeFinder(IEnumerable<Assembly> assemblies)
+        {
+            IEnumerable<Type> types = assemblies.SelectMany(x => x.GetTypes())
+                .Where(x => TYPE_LISTENER.IsAssignableFrom(x));
+            this.defaultFinder = new DefaultEventListenerTypeFinder(types);
+        }
+
+        public IEnumerable<Type> FindListenerTypesByEventType(Type eventType)
+        {
+            return this.defaultFinder.FindListenerTypesByEventType(eventType);
+        }
+    }
+}
